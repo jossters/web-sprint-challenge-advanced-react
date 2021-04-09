85b6faab371d89e3929134711d063a4f3c7d4fc4
Johnny Murillo <jossters@gmail.com>
2021-04-09 15:12:54 -0700

    all done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,35 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen} from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>);
+    const header = screen.queryByText(/checkout form/i);
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm/>);
+    const  first = screen.getByLabelText(/first name/i);
+    const  last = screen.getByLabelText(/last name/i);
+    const  address = screen.getByLabelText(/address/i);
+    const  city = screen.getByLabelText(/city/i);
+    const  state = screen.getByLabelText(/state/i);
+    const  zip = screen.getByLabelText(/zip/i);
+    const button = screen.getByRole("button",/checkout/i);
+
+    userEvent.type(first, "Johnny");
+    userEvent.type(last, "Murillo");
+    userEvent.type(address, "1234 W Arrow");
+    userEvent.type(city, "Covina");
+    userEvent.type(state, "CA");
+    userEvent.type(zip, "12345");
+    userEvent.click(button);
+
+        const sucsses = screen.getByTestId(/successMessage/i);
+        expect(sucsses).toBeTruthy();
+        
+});
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,23 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react';
+
+const useForm = (initialValue) => {
+    const [ values, setValue] = useState(initialValue);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+    const handleChanges = (e) => {
+        setValue({ 
+          ...values, 
+          [e.target.name]: e.target.value 
+        });
+      };
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+      };
+
+      return([ values,  showSuccessMessage, handleChanges, handleSubmit  ]);
+
+}
+export default useForm;
